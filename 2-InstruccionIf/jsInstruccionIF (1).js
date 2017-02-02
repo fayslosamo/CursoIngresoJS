36f8e652ec345955e85f9c2b323c9fc1c7a80aae
fayslosamo <fayslosamo@gmail.com>
2017-02-02 12:26:22 -0300

    carpeta 2

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,4 +1,13 @@
 function Mostrar()
 {
+var x = document.getElementById('edad');
 
+if (x = 15) 
+{
+	console.log("Niña Bonita");
+}
+if (x = 15) 
+{
+	alert("Niña Bonita");
+}
 }
--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,7 +1,14 @@
 function Mostrar()
 {
-//tomo la edad  
+//tomo la edad 
+var x = document.getElementById("edad").value;
 
-
-
-}//FIN DE LA FUNCIÓN
+if (x >= 18) 
+{
+	console.log("Mayor de edad");
+}
+if (x >= 18) 
+{
+	alert("Mayor de Edad");
+}
+}
